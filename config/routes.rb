cc853a87191310753fe458cc8a2e7a086367ced9
John Coote <john_coote@rocketmail.com>
2017-11-24 13:50:38 +1100

    tabvles and routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
+# == Route Map
+#
+#   Prefix Verb  URI Pattern              Controller#Action
+#     root GET   /                        pages#home
+#     jobs GET   /jobs(.:format)          jobs#index
+#          POST  /jobs(.:format)          jobs#create
+#  new_job GET   /jobs/new(.:format)      jobs#new
+# edit_job GET   /jobs/:id/edit(.:format) jobs#edit
+#      job PATCH /jobs/:id(.:format)      jobs#update
+#          PUT   /jobs/:id(.:format)      jobs#update
+# 
+
+
 Rails.application.routes.draw do
+
+
+
+  root :to => "pages#home"
+
+  resources :jobs, :only => [:index, :new, :create, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Booker.destroy_all
+booker1 = Booker.create :name => "Peter"
+booker2 = Booker.create :name => "Jane"
+
+Technician.destroy_all
+tech1 = Technician.create :name => "Stephen"
+tech2 = Technician.create :name => "Gillian"
+
+Job.destroy_all
+job1 = Job.create :summary => "Fix Leaking Tap"
+job2 = Job.create :summary => "Mow overgrown lawn"
+
+booker1.jobs << job1
+tech1.jobs << job1
+
+booker1.jobs << job1
+booker2.jobs << job2
