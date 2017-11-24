0663842eb2a66e7a3a41d3ca78e287ae69b55c71
John Coote <john_coote@rocketmail.com>
2017-11-24 15:36:58 +1100

    simple one table CRUD done

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,13 +1,39 @@
 class JobsController < ApplicationController
   def index
+    @jobs = Job.all
   end
 
   def new
+    @job = Job.new
   end
 
+  def create
+    job = Job.create job_params
+    redirect_to job
+  end
+
+
   def edit
+    @job = Job.find params[:id]
+  end
+
+  def update
+    job = Job.find params[:id]
+    job.update job_params
+    redirect_to job
   end
 
   def show
+    @job = Job.find params[:id]
   end
+
+
+
+  private
+  def job_params
+    params.require(:job).permit(:summary, :detail, :address, :requested_date, :accepted_date, :finished, :booker_id, :technician_id)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@
 # edit_job GET   /jobs/:id/edit(.:format) jobs#edit
 #      job PATCH /jobs/:id(.:format)      jobs#update
 #          PUT   /jobs/:id(.:format)      jobs#update
-# 
+#
 
 
 Rails.application.routes.draw do
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
   root :to => "pages#home"
 
-  resources :jobs, :only => [:index, :new, :create, :edit, :update]
+  resources :jobs, :only => [:index, :new, :create, :edit, :update, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
